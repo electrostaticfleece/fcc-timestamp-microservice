f04a8cbcedcfaf7bdcfa04e184781d155257cc4e
electrostaticfleece <trevorwong00@gmail.com>
2016-06-08 05:58:34 +0000

    Updated server.js to include timestamp microservice and updated index.jade to display correct unix values

--- server.js
+++ server.js
@@ -11,4 +11,15 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
+app.get(/\/.*/, function(request, response) {
+  var url = request.url.slice(1).replace(/\%20/g, " "),
+      rtnDate = !isNaN(url) && isFinite(url) ?  new Date(Date.UTC(1970, 0, 1, 0, 0, 0, +url*1000)) : new Date(url),
+      options = {year: 'numeric', month: 'long', day: 'numeric'},
+      natural = (rtnDate == ('Invalid Date' || null)) ? null : rtnDate.toLocaleDateString('en-US', options),
+      unixDate = !isNaN(url) ? Number(url) : (Date.parse(rtnDate)/1000 || null),
+      dateObj = JSON.stringify({'unix': unixDate, 'natural': natural});
+
+  response.end(dateObj);
+});
+
 http.createServer(app).listen(process.env.PORT);
